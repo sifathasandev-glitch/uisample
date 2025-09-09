7ee206e627d192575e92db3319b58b4373f9cf0a
sifat hasan <sifatshasan@gmail.com>
2025-09-09 12:55:12 +0600

    test 2

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
         Backgrounds 
       </p>
-      <h1>this is second</h1>
+      <h1>this is seconds</h1>
     </div>
   );
 }
