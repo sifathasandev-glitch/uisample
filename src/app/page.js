9beb0b53ef3e099ebaab96ab021fb95cefd90c03
sifat hasan <sifatshasan@gmail.com>
2025-09-09 12:50:21 +0600

    test

--- src/app/page.js
+++ src/app/page.js
@@ -16,8 +16,9 @@ export default function Home() {
       {/* Radial gradient for the container to give a faded look */}
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
       <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-        Backgrounds
+        Backgrounds 
       </p>
+      <h1>this is second</h1>
     </div>
   );
 }
